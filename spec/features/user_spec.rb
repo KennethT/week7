a1d4c1aefef6caaf59ba689b5d9e0ed9560902fb
Kenneth To <kennethkto@gmail.com>
2015-02-23 15:21:47 -0700

    add rspec test for valid user creation

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,18 +3,17 @@ require 'rails_helper'
 describe 'User can CRUD users' do
 
   scenario 'User can create a user' do
-    visit '/'
 
-    click_on "Register"
+    visit('/registrations/new')
 
+    click_on 'Register'
 
     fill_in 'user[user_name]', :with => "Cow"
     fill_in 'user[about]', :with => "curd"
     fill_in 'user[password]', :with => "milk"
     fill_in 'user[password_confirmation]', :with => "milk"
 
-    click('Sign Up')
-
+    click_on "Sign Up"
 
     expect(page).to have_content("Cow")
     expect(page).to have_content("curd")
