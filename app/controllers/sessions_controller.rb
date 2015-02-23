b419d15dc8da911c3a211e281b4cee19a221484a
Kenneth To <kennethkto@gmail.com>
2015-02-23 12:08:01 -0700

    fix typo in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(user_name: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to root_path, notice: 'Welcome!'
